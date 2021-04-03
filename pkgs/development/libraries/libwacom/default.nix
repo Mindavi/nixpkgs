50b0a6cd075264f12a28f241dd5aa9fc308a887b
Rick van Schijndel <rol3517@gmail.com>
2021-04-03 12:43:53 +0200

    libwacom: fix cross compilation by using a development version
    This should not land in the nixpkgs tree, but it's useful for me now.

--- pkgs/development/libraries/libwacom/default.nix
+++ pkgs/development/libraries/libwacom/default.nix
@@ -1,19 +1,21 @@
-{ lib, stdenv, fetchFromGitHub, meson, ninja, glib, pkg-config, udev, libgudev, doxygen }:
+{ lib, stdenv, fetchFromGitHub, meson, ninja, glib, pkg-config, udev, libgudev, doxygen, python3 }:
 
 stdenv.mkDerivation rec {
   pname = "libwacom";
-  version = "1.9";
+  version = "unstable-2021-04-06";
 
   outputs = [ "out" "dev" ];
 
   src = fetchFromGitHub {
     owner = "linuxwacom";
     repo = "libwacom";
-    rev = "libwacom-${version}";
-    sha256 = "sha256-o1xCSrWKPzz1GePEVB1jgx2cGzRtw0I6c4wful08Vx4=";
+    #rev = "libwacom-${version}";
+    rev = "327f1258057c45c4f6fbb2133842c30f1969b3ea";
+    #sha256 = "sha256-o1xCSrWKPzz1GePEVB1jgx2cGzRtw0I6c4wful08Vx4=";
+    sha256 = "sha256-mBEh+ZtuPFm7Ie1peohtHTnjx3/pv0FRkIaYvswfkK0=";
   };
 
-  nativeBuildInputs = [ pkg-config meson ninja doxygen ];
+  nativeBuildInputs = [ pkg-config meson ninja doxygen python3 ];
 
   mesonFlags = [ "-Dtests=disabled" ];
 
