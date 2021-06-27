e1ece2d0daffd6a8563e11ca465b8d83f6d0da80
Rick van Schijndel <rol3517@gmail.com>
2021-06-27 23:36:33 +0200

    llvmPackages_latest: temporarily revert to llvm 11

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -11629,7 +11629,7 @@ in
     stdenv = gcc7Stdenv;
   }));
 
-  llvmPackages_latest = llvmPackages_12;
+  llvmPackages_latest = llvmPackages_11;
 
   llvmPackages_rocm = recurseIntoAttrs (callPackage ../development/compilers/llvm/rocm { });
 
