56cf322fe63e9f8dfbe58389a137cc3a5d11b294
Rick van Schijndel <rol3517@gmail.com>
2023-04-11 20:30:07 +0200

    pkg-config: use u-config as implementation
    Just for trying this out, seems pretty interesting to verify how well it works.

--- pkgs/development/tools/misc/pkg-config/default.nix
+++ pkgs/development/tools/misc/pkg-config/default.nix
@@ -5,40 +5,34 @@ stdenv.mkDerivation rec {
   version = "0.29.2";
 
   src = fetchurl {
-    url = "https://pkg-config.freedesktop.org/releases/${pname}-${version}.tar.gz";
-    sha256 = "14fmwzki1rlz8bs2p810lk6jqdxsk966d8drgsjmi54cd00rrikg";
+    #url = "https://pkg-config.freedesktop.org/releases/${pname}-${version}.tar.gz";
+    url = "https://github.com/skeeto/u-config/archive/refs/tags/v0.31.0.tar.gz";
+    hash = "sha256-XgDqPCH8hqZqReLOew7YkPeY4P3RbaVZGcNRI0Lr61s=";
   };
 
-  outputs = [ "out" "man" "doc" ];
+  outputs = [ "out" ];
   strictDeps = true;
-
-  # Process Requires.private properly, see
-  # http://bugs.freedesktop.org/show_bug.cgi?id=4738, migrated to
-  # https://gitlab.freedesktop.org/pkg-config/pkg-config/issues/28
-  patches = lib.optional (!vanilla) ./requires-private.patch
-    ++ lib.optional stdenv.isCygwin ./2.36.3-not-win32.patch;
-
-  # These three tests fail due to a (desired) behavior change from our ./requires-private.patch
-  postPatch = if vanilla then null else ''
-    rm -f check/check-requires-private check/check-gtk check/missing
-  '';
 
   buildInputs = [ libiconv ];
 
-  configureFlags = [ "--with-internal-glib" ]
-    ++ lib.optionals (stdenv.isSunOS) [ "--with-libiconv=gnu" "--with-system-library-path" "--with-system-include-path" "CFLAGS=-DENABLE_NLS" ]
-       # Can't run these tests while cross-compiling
-    ++ lib.optionals (stdenv.hostPlatform != stdenv.buildPlatform)
-       [ "glib_cv_stack_grows=no"
-         "glib_cv_uscore=no"
-         "ac_cv_func_posix_getpwuid_r=yes"
-         "ac_cv_func_posix_getgrgid_r=yes"
-       ];
+  buildPhase = ''
+    runHook preBuild
+
+    $CC -Os -o pkg-config generic_main.c
+
+    runHook postBuild
+  '';
 
   enableParallelBuilding = true;
   doCheck = true;
 
-  postInstall = ''rm -f "$out"/bin/*-pkg-config''; # clean the duplicate file
+  installPhase = ''
+    runHook preInstall
+
+    install -Dm755 pkg-config $out/bin/pkg-config
+
+    runHook postInstall
+  '';
 
   meta = with lib; {
     description = "A tool that allows packages to find out information about other packages";
